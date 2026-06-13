11ec827cf9ede743a0052e9510433d5e1b31a883
surbhii-25 <1surbhichoudhary@gmail.com>
2026-06-13 17:08:57 +0530

    routing and navigation added to home page and else

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,24 @@
+import {Fragment} from 'react'
+import { Outlet, Link} from 'react-router';
+import './navigation.styles.scss'
+import CrwnLogo from '../../assets/crown.svg?react'
+
+const Navigation = () => {
+    return(
+        <Fragment>
+            <div className="navigation">
+                <Link className='logo-container' to='/'>
+                    <CrwnLogo className="logo"/>
+                </Link>
+                <div className='nav-links-container'>
+                    <Link className='nav-link' to='/shop'>
+                        SHOP
+                    </Link>
+                </div>
+            </div>
+            <Outlet/>
+        </Fragment>
+    )
+}
+
+export default Navigation;
