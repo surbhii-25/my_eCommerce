77be435f7d4209f1adf1d041b5f6dcb36b57057b
surbhii-25 <1surbhichoudhary@gmail.com>
2026-06-22 23:35:45 +0530

    shop-page through product-context product-card

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,7 @@ import {Routes, Route} from 'react-router';
 import Navigation from './routes/navigation/navigation.component'
 import Authentication from './routes/authentication/authentication.component'
 import Home from './routes/home/home.component';
-
-const Shop = () => {
-    return(
-        <h1>I am the Shop page</h1>
-    )
-}
+import Shop from './routes/shop/shop.component'
 
 const App = () => {
     return (
