56cfd11f99d8ef024f148dcac71bf17b42d73147
surbhii-25 <1surbhichoudhary@gmail.com>
2026-06-25 17:39:09 +0530

    dropDown completed with total showed in bagIcon

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,20 @@
+import {useContext} from 'react';
 import Button from '../button/button.component'
-
+import { CartContext } from '../../contexts/cart.context'
+import CartItem from '../cart-item/cart-item.component'
 import './cart-dropdown.styles.scss'
 
 const CartDropdown = () => {
+    const {cartItems} = useContext(CartContext);
     return(
         <div className='cart-dropdown-container'>
             <div className='cart-items' />
+            {cartItems.length ? (
+                cartItems.map((cartItem)=>(
+                    <CartItem key = {cartItem.id} cartItem={cartItem} />
+                ))):(
+                    <span className='empty-message'>Your Cart is Empty</span>
+                )}
             <Button>GO TO CHECKOUT</Button>
         </div>
     )
