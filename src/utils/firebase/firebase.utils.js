f47565e32bd0dd7e6566bc44a69df235305487d8
surbhii-25 <1surbhichoudhary@gmail.com>
2026-06-26 20:38:02 +0530

    firestore collection only time created

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -12,7 +12,9 @@ import {
   getFirestore,
   doc,
   getDoc,
-  setDoc
+  setDoc,
+  collection,
+  writeBatch
 } from 'firebase/firestore'
 
 const firebaseConfig = {
@@ -37,6 +39,19 @@ export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 
 export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) =>{
+  const batch = writeBatch(db);
+  const collectionRef = collection(db, collectionKey);
+
+  objectsToAdd.forEach((object)=>{
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object);
+  });
+
+  await batch.commit();
+  console.log('done')
+}
 
 export const createUserDocumentFromAuth = async (userAuth, additionalInformation={}) => {
   if(!userAuth) return;
